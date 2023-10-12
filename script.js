c3871121373778fc22ee9f2f891f53c1d1392a52
CosmicGold5922 <146548025+CosmicGold5922@users.noreply.github.com>
2023-10-13 01:35:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function next1(){
+    document.querySelector('.div-vec-no-text').classList.remove('no-body')
+}
+function next2(){
+    document.querySelector('.div-vec-no-text').classList.add('no-body')
+}
+function next3(){
+    document.querySelector('.div-vec-small1-card-text-section2').classList.remove('no-body')
+}
+function next4(){
+    document.querySelector('.div-vec-small1-card-text-section2').classList.add('no-body')
+}
+function next5(){
+    document.querySelector('.div-vec-small2-card-text-section2').classList.remove('no-body')
+}
+function next6(){
+    document.querySelector('.div-vec-small2-card-text-section2').classList.add('no-body')
+}
